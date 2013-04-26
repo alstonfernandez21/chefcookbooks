cc840e4ffc5a724764388b23a3afeaa7badadc14
alston fernandez <alston.fernandez@gmail.com>
2013-04-26 13:47:43 -0400

    added support for centos 6 yum packages

--- php53fcgi/attributes/default.rb
+++ php53fcgi/attributes/default.rb
@@ -1,1 +1,8 @@
-default['php53fcgi']['packages'] = [ "php53", "php53-cli", "php53-common", "php53-gd", "php53-xml", "php53-mbstring", "php53-mcrypt", "php53-pdo", "php53-devel", "php53-mysql" ]
+case node[:platform]
+when "centos"
+ if node[:platform_version].to_f < 6
+  default['php53fcgi']['packages'] = [ "php53", "php53-cli", "php53-common", "php53-gd", "php53-xml", "php53-mbstring", "php53-mcrypt", "php53-pdo", "php53-devel", "php53-mysql" ]
+ else
+  default['php53fcgi']['packages'] = ["php", "php-devel", "php-cli", "php-common", "php-gd", "php-xml", "php-mbstring", "php-mcrypt", "php-pdo", "php-mysql" ]   
+ end
+end
